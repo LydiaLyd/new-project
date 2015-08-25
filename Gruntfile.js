ef3f389007d6780352fb66c33ea12b8f06dd84c8
Lydia <ridea@bk.ru>
2015-08-25 21:11:47 +0300

    Correct Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,78 +1,132 @@
 module.exports = function(grunt) {
 
+  // загрузить задачи из package.json
   require("load-grunt-tasks")(grunt);
 
   grunt.initConfig({
- 
-    clean: {
-      build: ["build"]
-    },
 
-    copy: {
-      build: {
-        files: [{
-          expand: true,
-          cwd: "source",
-          src: [
-            "index.html",
-            "img/*.{png,jpg,gif,svg}",
-            "font"
-          ],
-          dest: "build"
-        }]
-      }
-    },
 
+
+    // скомпилировать style.less в style.css
     less: {
-      styleBuild: {
-        files: {
-          "build/css/style.css": ["source/less/style.less"],
-        }
-      },
-      styleDevelopment: {
+
+      develop: {
         files: {
           "source/css/style.css": ["source/less/style.less"],
         }
       },
+
+      build: {
+        files: {
+          "build/css/style.css": ["source/less/style.less"],
+        }
+      },
+
     },
 
+
+
+    // добавить префиксы
     autoprefixer: {
       options: {
         browsers: ["last 2 version", "ie 10"]
       },
-      style: {
+      build: {
         src: "build/css/style.css"
       },
     },
 
+
+
+    // объединить медиавыражения ?
     combine_mq: {
-      style: {
+      build: {
         src: "build/css/style.css",
         dest: "build/css/style.css"
       }
     },
 
-    cssmin: {
-      styleBuild: {
+
+
+    // изменить пути ? непонятно, зачем вообще мне это нужно
+    replace: {
+      build: {
         options: {
-          keepSpecialComments: 0,
-          report: "gzip"
+          patterns: [{
+            match: /[\"\']img\//g,
+            replacement: '"/static/img'
+          }, {
+            match: /[\"\']css\//g,
+            replacement: '"/static/css'
+          }, {
+            match: /[\"\']js\//g,
+            replacement: '"/static/js' 
+          }]
         },
-        files: {
-          "build/css/style.min.css": ["build/css/style.css"],
-        }
-      },
-      styleDevelopment: {
+        files: [{
+          expand: true,
+          src: [
+            "build/css/style.css",
+            "build/*.html"
+          ]
+        }]
+      }
+    },
+
+
+
+    // ужать style.css
+    cssmin: {
+
+      develop: {
         options: {
           keepSpecialComments: 0,
-          report: "gzip"
         },
         files: {
           "source/css/style.min.css": ["source/css/style.css"],
         }
       },
+
+      build: {
+        options: {
+          keepSpecialComments: 0,
+        },
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"],
+        }
+      },
+
     },
 
+
+
+    // "причесать" less-файлы
+    csscomb: {
+      style: {
+        expand: true,
+        src: [
+          "source/less/*.less",
+          "source/less/components/*.less",
+        ]
+      }
+    },
+
+
+
+    // сделать спрайт
+    sprite: {
+      all: {
+        src: 'source/img/sprites/*.png',
+        dest: 'source/img/spritesheet.png',
+        destCss: 'source/less/sprites.less',
+        algorithm: 'top-down',
+        padding: 10,
+      }
+    },
+
+
+
+    // оптимизировать изображения
     imagemin: {
       images: {
         options: {
@@ -85,135 +139,138 @@ module.exports = function(grunt) {
       }
     },
 
-//    htmlmin: {
-//      options: {
-//        removeComments: true,
-//        collapseWhitespace: true,
-//        collapseBooleanAttributes: true,
-//        caseSensitive: true,
-//        keepClosingSlash: false
-//      },
-//      html: {
-//        files: {
-//          "build/index.min.html": "build/index.html"
-//        }
-//      }
-//    },
 
-//    replace: {
-//      build: {
-//        options: {
-//          patterns: [{
-//            match: /[\"\']img\//g,
-//            replacement: '"/static/img'
-//          }, {
-//            match: /[\"\']css\//g,
-//            replacement: '"/static/css'
-//          }, {
-//            match: /[\"\']js\//g,
-//            replacement: '"/static/js' 
-//          }]
-//        },
-//        files: [{
-//          expand: true,
-//          src: [
-//            "build/css/style*.css",
-//            "build/index*.html"
-//          ]
-//        }]
-//      }
-//    },
 
-//    concat: {
-//      scriptBuild: {
-//        src: 'source/js/*.js',
-//        dest: 'build/js/script.js',
-//      },
-//      scriptDevelopment: {
-//        src: 'source/js/*.js',
-//        dest: 'source/js/script.js',
-//      },
-//    },
+    // объединить js-файлы
+    concat: {
 
-    uglify: {
-      scriptBuild: {
-        src: 'build/js/script.js',
-        dest: 'build/js/script.min.js',
+      develop: {
+        src: [
+          'source/js/script.js',
+          'source/js/plagins/*.js',
+        ],
+        dest: 'source/js/script.js',
       },
-      scriptDevelopment: {
+
+      build: {
+        src: [
+          'source/js/script.js',
+          'source/js/plagins/*.js',
+        ],
+        dest: 'build/js/script.js',
+      },
+
+    },
+
+
+
+    // ужать js-файлы
+    uglify: {
+
+      develop: {
         src: 'source/js/script.js',
         dest: 'source/js/script.min.js',
       },
+      
+      build: {
+        src: 'build/js/script.js',
+        dest: 'build/js/script.min.js',
+      },
+
+      
     },
 
-    csscomb: {
-      style: {
-        expand: true,
-        src: ["source/less/**/*.less"]
-      }
-    },
 
-    sprite: {
-      all: {
-        src: 'source/img/sprites/*.png',
-        dest: 'source/img/spritesheet.png',
-        destCss: 'source/css/sprites.css',
-        algorithm: 'top-down',
-        padding: 10,
-      }
-    },
 
+    // следить за изменениями и обновлять style.min.css, спрайт и sript.min.js
     watch: {
-      less: {
+
+      style: {
         files: [
           "source/less/*.less",
-          "source/less/components/*.less"
+          "source/less/components/*.less",
         ],
-        tasks: ['less:styleDevelopment', 'cssmin:styleDevelopment',],
+        tasks: [
+          'less:develop',
+          'cssmin:develop',
+        ],
         options: {
           spawn: false,
-        }
+        },
       },
+
       sprite: {
         files: ['source/img/sprites/*.png'],
         tasks: ['sprite'],
         options: {
           spawn: false,
-        }
+        },
       },
-      },
+
       scripts: {
-        files: ['source/js/*.js'],
+        files: [
+          'source/js/*.js',
+          'source/js/plugins/*.js'
+        ],
         tasks: [
-//          'concat:scriptDevelopment', 
-          'uglify:scriptDevelopment'
+          'concat:develop', 
+          'uglify:develop'
         ],
         options: {
             spawn: false,
         },
       },
-    });
+
+    },
+
+
+
+    // удалить папку build
+    clean: {
+      build: ["build"]
+    },
+
+
+
+    // скопировать из папки source в папку build все html-файлы, папку с изображениями и папку со шрифтами
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: [
+            "*.html",
+            "img/*.{png,jpg,gif,svg}",
+            "font"
+          ],
+          dest: "build"
+        }]
+      }
+    },
+
+
+
+  });
 
   grunt.registerTask('build', [
-    "clean",
-    "copy",
-    "less:styleBuild",
-    "autoprefixer",
-    "combine_mq",
-    "cssmin:styleBuild",
-    "imagemin",
-//    "htmlmin",
+    "clean",  // удалить папку build
+    "copy",  // скопировать из папки source в папку build все html-файлы, папку с изображениями и папку со шрифтами
+    "less:build",  // скомпилировать source/less/style.less в build/css/style.css
+    "autoprefixer",  // добавить префиксы
+    "combine_mq",  // объединить медиавыражения ?
+    "cssmin:build",  // ужать style.css
+    "imagemin",  // оптимизировать изображения
 //    "replace",
-//    "concat:scriptBuild",
-    "uglify:scriptBuild"
+    "concat:build",  // объединить js-файлы
+    "uglify:build",  // ужать js-файлы
   ]);
   
-  grunt.registerTask('development', [
-    "less:styleDevelopment",
-    "cssmin:styleDevelopment",
-    "sprite",
-//    "concat:scriptDevelopment",
-    "uglify:scriptDevelopment"
+  grunt.registerTask('develop, [
+    "sprite",  // создать spritesheet.png и sprites.less
+    "less:develop",  // скомпилировать source/less/style.less в source/css/style.css
+    "cssmin:develop",  // ужать style.css
+    "concat:develop",  // объединить js-файлы
+    "uglify:develop",  // ужать js-файлы
   ]);
 
 };
